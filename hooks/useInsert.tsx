f6182eea79a71ad0134c7432e6458035a679fd67
Mamadou <20203997@etud.univ-evry.fr>
2022-10-18 09:58:16 +0200

    manage creating user account

--- hooks/useInsert.tsx
+++ hooks/useInsert.tsx
@@ -5,7 +5,7 @@ const insertData = (uri: string, { data, options }: any) => {
   return api.post(uri, data, options);
 };
 
-export const useInsert = (uri: string, properties: object) => {
+export const useInsert = (uri: string, properties: object = {}) => {
   return useMutation((options: object) => insertData(uri, options), {
     ...properties,
   });
