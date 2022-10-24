88946ddaf08e41ab68b337cb64f13b1566ffdb0e
Mamadou <20203997@etud.univ-evry.fr>
2022-10-24 11:38:13 +0200

    upload a video

--- components/videoUploadForm/VideoUploadForm.tsx
+++ components/videoUploadForm/VideoUploadForm.tsx
@@ -1,9 +1,13 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import { Grid, TextareaAutosize, Button } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { useFetch } from "../../hooks/useFetch";
+import { useInsert } from "../../hooks/useInsert";
+import tokenService from "../../services/token.service";
+import Alert from "@mui/material/Alert";
+import { LoadingBackdrop } from "../LoadingBackDrop";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -13,26 +17,40 @@ const useStyles = makeStyles(() => ({
   textFields: {
     width: "100% !important",
   },
+  alert: {
+    margin: "10px 0 10px 0",
+  },
 }));
 
 export const VideoUploadForm = () => {
   const classes = useStyles();
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
-  const [category, setcategory] = useState<string>("Choisir une catégorie");
+  const [categoryId, setcategoryId] = useState<string>("Choisir une catégorie");
   const [video, setVideo] = useState<File>();
   const [videoThumbnail, setVideoThumbnail] = useState<File>();
+  const [userId, setUserId] = useState<string>();
+  const [insertedVideoId, setInsertedVideoId] = useState<string>();
+  const [alert, setAlert] = useState<string>();
 
   const { data } = useFetch("/categories", {
     staleTime: Infinity,
   });
+  const {
+    mutate: insertVideo,
+    isLoading: isVideoUploadLoading,
+    isError,
+  } = useInsert(`/user/${userId}/videos`);
+  const { mutate: insertThumbnail } = useInsert(
+    `/user/${userId}/videos/${insertedVideoId}/thumbnail`
+  );
 
   const handleVideoTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
 
-  const handleCategoryChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setcategory(e.target.value);
+  const handleCategoryIdChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setcategoryId(e.target.value);
   };
 
   const handleVideoUpload = (e: ChangeEvent<HTMLInputElement>) => {
@@ -50,8 +68,78 @@ export const VideoUploadForm = () => {
   };
 
   const saveVideo = () => {
-    console.log({ title, description, category, video, videoThumbnail });
+    // console.log({ title, description, categoryId, video, videoThumbnail });
+    if (userId !== undefined && userId !== null) {
+      if (
+        video !== undefined &&
+        video !== null &&
+        videoThumbnail !== undefined &&
+        videoThumbnail !== null
+      ) {
+        const videoForm = new FormData();
+        videoForm.append("file", video);
+        insertVideo(
+          {
+            data: videoForm,
+            options: {
+              headers: {
+                "Content-Type": "multipart/form-data",
+              },
+              params: { title, description, categoryId },
+            },
+          },
+          {
+            onSuccess: (data) => {
+              setInsertedVideoId(data.data.lastUploadedFileId);
+              // setInsertedVideoId(
+              //   (prev) => (prev!["insertedVideoId"] = data.data._id)
+              // );
+              if (insertedVideoId !== undefined) {
+                console.log("insertedVideoId", insertedVideoId);
+                const thumbnailForm = new FormData();
+                thumbnailForm.append("file", videoThumbnail);
+                insertThumbnail(
+                  {
+                    data: thumbnailForm,
+                    options: {
+                      headers: { "Content-Type": "multipart/form-data" },
+                    },
+                    params: { title, description, categoryId },
+                  },
+                  {
+                    onSuccess: (data) => {
+                      console.log(data);
+                    },
+                  }
+                );
+              }
+            },
+          }
+        );
+      } else {
+        setAlert("Veuillez choisir une video et une miniature");
+        setTimeout(() => {
+          setAlert("");
+        }, 3000);
+      }
+    } else {
+      setAlert("Vous devez être connecté pour publier une vidéo");
+      setTimeout(() => {
+        setAlert("");
+      }, 3000);
+    }
   };
+
+  useEffect(() => {
+    const user = tokenService.getUser();
+    if (user && user.userId) {
+      setUserId(user.userId);
+    }
+  }, []);
+
+  if (isError) {
+    return <div>Une erreur est survenue</div>;
+  }
 
   return (
     <div className={classes.root}>
@@ -80,16 +168,16 @@ export const VideoUploadForm = () => {
           <Grid item xs={12} sm={6}>
             <TextField
               className={classes.textFields}
-              id="outlined-select-category"
+              id="outlined-select-categoryId"
               select
               label="Catégorie"
-              value={category}
-              onChange={handleCategoryChange}
+              value={categoryId}
+              onChange={handleCategoryIdChange}
             >
-              {data?.data?.categories.map((category: any) => {
-                const { _id, name } = category;
+              {data?.data?.categories.map((categoryId: any) => {
+                const { _id, name } = categoryId;
                 return (
-                  <MenuItem key={_id} value={name}>
+                  <MenuItem key={_id} value={_id}>
                     {name}
                   </MenuItem>
                 );
@@ -140,9 +228,15 @@ export const VideoUploadForm = () => {
           </Grid>
         </Grid>
       </Grid>
+      {alert && (
+        <Alert severity="error" className={classes.alert}>
+          {alert}
+        </Alert>
+      )}
       <Button variant="contained" onClick={saveVideo}>
         Publier
       </Button>
+      <LoadingBackdrop open={isVideoUploadLoading} />
     </div>
   );
 };
