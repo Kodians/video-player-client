4c93fbd7d6fbf0c4f61d2435694cfc1a480c4e44
Mamadou <20203997@etud.univ-evry.fr>
2022-10-28 23:09:18 +0200

    List user videos and load more progressively

--- components/userVideosInfos/userVideosInfos.tsx
+++ components/userVideosInfos/userVideosInfos.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { List, ListItem } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import Image from "next/image";
@@ -5,6 +6,9 @@ import videoIcon from "../../assets/images/video.svg";
 import React from "react";
 import { ActionPopover } from "./ActionPopover";
 import Link from "next/link";
+import tokenService from "../../services/token.service";
+import { useFetch } from "../../hooks/useFetch";
+import { useInfiniteFetch } from "../../hooks/useInfiniteFetch";
 
 const useStyles = makeStyles(() => ({
   listItem: {
@@ -12,94 +16,98 @@ const useStyles = makeStyles(() => ({
     justifyContent: "space-between",
     alignItems: "center",
     width: "100%",
-    minWidth: 400,
     "& >:nth-child(n+1)": {
       marginLeft: 20,
+      padding: 10,
+    },
+    "& >:nth-child(1), & >:nth-child(2), & >:nth-child(5)": {
+      width: "calc(100% / 100)",
+    },
+    "& >:nth-child(3), & >:nth-child(4)": {
+      width: "calc(100% / 3)",
     },
   },
 }));
-const userVideosInfos = [
-  {
-    id: 1,
-    title: "This is a Video 1",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 2,
-    title: "This is a Video 2",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 3,
-    title: "This is a Video 3",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 4,
-    title: "This is a Video 4",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 5,
-    title: "This is a Video 5",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 6,
-    title: "This is a Video 6",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 7,
-    title: "This is a Video 7",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 8,
-    title: "This is a Video 8",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 9,
-    title: "This is a Video 9",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    id: 10,
-    title: "This is a Video 10",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-];
+
 const UserVideosInfos = () => {
   const classes = useStyles();
+
+  const [userId, setUserId] = React.useState<string>();
+
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    isFetching,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteFetch(`/user/${userId}/videos/metadata/info`, {
+    enabled: !!userId,
+    staleTime: Infinity,
+    getNextPageParam: (_lastPage: any, pages: any) => {
+      if (pages.length < 2) {
+        return pages.length + 1;
+      }
+      return undefined;
+    },
+  });
+
+  useEffect(() => {
+    const user = tokenService.getUser();
+    if (user && user.userId) {
+      setUserId(user.userId);
+    }
+  }, []);
+
+  console.log(data?.pages);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
   return (
-    <List>
-      {userVideosInfos.map((video) => {
-        return (
-          <ListItem key={video.id} className={classes.listItem} divider>
-            <p>{video.id}</p>
-            <p>
-              <Link href={"/"}>
-                <a>
-                  <Image
-                    src={videoIcon}
-                    layout="fixed"
-                    width={20}
-                    height={20}
-                    alt="video.svg"
-                  />
-                </a>
-              </Link>
-            </p>
-            <p>{video.title}</p>
-            <p>{video.description}</p>
-            <p>
-              <ActionPopover />
-            </p>
-          </ListItem>
-        );
-      })}
-    </List>
+    <>
+      <List>
+        {data?.pages.map((page: any) => {
+          return page.data.map((video: any, index: any) => {
+            const { videoId, title, description } = video;
+            return (
+              <ListItem key={title} className={classes.listItem} divider>
+                <p>{index + 1}</p>
+                <p>
+                  <Link href={`/videos/${videoId}`}>
+                    <a>
+                      <Image
+                        src={videoIcon}
+                        layout="fixed"
+                        width={20}
+                        height={20}
+                        alt="video.svg"
+                      />
+                    </a>
+                  </Link>
+                </p>
+                <p>{title}</p>
+                <p>{description}</p>
+                <p>
+                  <ActionPopover />
+                </p>
+              </ListItem>
+            );
+          });
+        })}
+        <ListItem sx={{ display: "flex", justifyContent: "center" }}>
+          <div>{isFetching && isFetchingNextPage ? "Fetching..." : null}</div>
+        </ListItem>
+      </List>
+      <button disabled={!hasNextPage} onClick={fetchNextPage}>
+        Load More
+      </button>
+    </>
   );
 };
 
