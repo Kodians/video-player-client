430d79b8f87e48472d11649184785b1896d8237f
Mamadou <20203997@etud.univ-evry.fr>
2022-12-06 10:58:37 +0100

    configure github actions

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -5,6 +18,8 @@ const nextConfig = {
   images: {
     domains: ["i.ibb.co"],
   },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 module.exports = nextConfig;
