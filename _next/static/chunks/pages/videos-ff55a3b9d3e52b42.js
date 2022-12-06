ca4c6140946ba713cbbac00a25c82bcb7f8f91d2
mamadou-niakate <mamadouniakate10@yahoo.fr>
2022-12-06 15:11:26 +0000

    Deploying to gh-pages from @ Kodians/video-player-client@e89b2b9d6c6f1f3c655d2e492d352d4ee690f3bd 🚀

---
+++ _next/static/chunks/pages/videos-ff55a3b9d3e52b42.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[52],{8162:function(n,u,i){"use strict";i.r(u),i(7294);var e=i(5893);u.default=function(){return(0,e.jsx)("div",{children:"Videos"})}},8484:function(n,u,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/videos",function(){return i(8162)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8484)}),_N_E=n.O()}]);
--- pages/videos/user/[userId].tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function UserVideos() {
-  return <div>UserVideos</div>;
-}
-
-export default UserVideos;
