34c5b4cfb00f92cd89cba23b08d7dcfdeed3ccb3
Mamadou <20203997@etud.univ-evry.fr>
2022-12-22 23:02:05 +0100

    fix basePath issue

--- next.config.js
+++ next.config.js
@@ -3,20 +3,10 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false;
 let assetPrefix = "";
 let basePath = "/";
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+let nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
     loader: "imgix",
     path: "https://beingdev.imgix.net/",
@@ -26,5 +16,19 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: "http://localhost:3000",
   },
 };
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+
+  nextConfig = {
+    ...nextConfig,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+  };
+}
 
 module.exports = nextConfig;
